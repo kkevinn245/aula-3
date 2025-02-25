002d639b7e94d88dc9e4723872701f03d907a3b7
kkevinn245 <kc459655@gmail.com>
2025-02-25 20:19:36 -0300

    calculo aula 3

---
+++ aula.3.py
@@ -0,0 +1,21 @@
+def funcao():
+    a = 5
+    b = 7
+    print(a+b)
+
+#funcao()
+
+def somar(a, b):
+    print(a+b)
+somar(5, 7)
+
+def divide(a, b):
+    if (b == 0):
+        print ("divisao por 0")
+    else:
+        print(f'O resultado da divisao entre: {a} e {b} e: {a/b}')
+divide(5, 5)
+
+def multiplicar(a, b):
+    print(a*b)
+multiplicar(5, 5)
