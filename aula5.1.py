a2435c12b60d1e9467623cfbb3d1166ee826c185
unknown <kc459655@gmail.com>
2025-03-14 22:03:28 -0300

    recursivos

--- aula5.1.py
+++ aula5.1.py
@@ -1,5 +1,11 @@
-fatorial(int a)
-  if a = 0 
-    return 1
-else 
-    return a * fatorial(a-0)
+def fatorial (a):
+    if a == 0:
+     return 1
+    else:
+     print(a)
+     return a * fatorial(a-1)
+numero = 1
+entrada = int(input("numero parra calcular o fatorial"))
+while numero <= entrada:
+    print(f"fatorial de : {numero}: {fatorial(numero)}")
+    numero = numero + 1 
